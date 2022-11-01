f696883e3b23dbd642e81e49638c85c6241205ec
Matthew DeGraffenreid <matthew.degraffenreid@cresta.ai>
2022-10-31 22:31:33 -0600

    adding new controllers

---
+++ application/src/main/java/ai/cresta/controller/Five9Controller.java
@@ -0,0 +1,11 @@
+package ai.cresta.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/five9")
+public class Five9Controller {
+    
+}
---
+++ application/src/main/java/ai/cresta/controller/GenesysMessagingController.java
@@ -0,0 +1,10 @@
+package ai.cresta.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/genesys")
+public class GenesysMessagingController {
+    
+}
