016c9eef443b9f725af0ce129bdef52737e527e8
Matthew DeGraffenreid <matthew.degraffenreid@cresta.ai>
2022-11-01 17:38:44 -0600

    Add controller endpoints

--- application/src/main/java/ai/cresta/controller/GenesysChatController.java
+++ application/src/main/java/ai/cresta/controller/GenesysChatController.java
@@ -1,11 +1,36 @@
 package ai.cresta.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import ai.cresta.data.UserDto;
+import ai.cresta.ports.api.MessageServicePort;
 
 
 @RestController
 @RequestMapping("/genesys")
 public class GenesysChatController {
     
+    @Autowired
+    private MessageServicePort messageServicePort;
+
+    @PostMapping("/send-chat")
+    public void sendChat(@RequestBody final String message){
+        messageServicePort.notifyFrontend("message");
+    }
+
+    @PostMapping("/send-user-chat/{id}")
+    public void sendUserChat(@PathVariable final String id,
+                             @RequestBody final String message){
+        messageServicePort.notifyUser(id, "message");
+    }
+
+    @PostMapping("/webChat/accessToken")
+    public ResponseEntity storeAccessToken(@RequestBody final UserDto userDto ){
+        messageServicePort.setAccessToken(userDto.getUserId(), userDto.getAccessToken());
+        return ResponseEntity.ok().build();
+    }
+
 }
