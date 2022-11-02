4b2ea3d61ca4e68363db685f7bd6c5f7e7d9fe12
Matthew DeGraffenreid <matthew.degraffenreid@cresta.ai>
2022-11-01 20:23:33 -0600

    Update MessageServicePort.java

--- domain/src/main/java/ai/cresta/ports/api/MessageServicePort.java
+++ domain/src/main/java/ai/cresta/ports/api/MessageServicePort.java
@@ -1,10 +1,12 @@
 package ai.cresta.ports.api;
 
+import ai.cresta.data.ConversationSubscriptionRequestDto;
+
 public interface MessageServicePort {
 
     public void notifyFrontend(final String messages);
 
     public void notifyUser(final String id, final String messages);
 
-    public void setAccessToken(String userId, String accessToken);
+    public void subscribeToConversation(ConversationSubscriptionRequestDto conversationSubscriptionRequestDto);
 }
