8c9086db19f90a748d54216805eb36e69e6b0507
Matthew DeGraffenreid <matthew.degraffenreid@cresta.ai>
2022-11-01 21:22:20 -0600

    Set up web messaging

--- application/src/main/java/ai/cresta/controller/GenesysMessagingController.java
+++ application/src/main/java/ai/cresta/controller/GenesysMessagingController.java
@@ -1,10 +1,34 @@
 package ai.cresta.controller;
 
+import ai.cresta.data.ConversationSubscriptionRequestDto;
+import ai.cresta.data.ConversationSubscriptionResponseDto;
+import ai.cresta.data.UserDto;
+import ai.cresta.ports.api.MessageServicePort;
+import ai.cresta.ports.api.UserServicePort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/genesys")
 public class GenesysMessagingController {
-    
+    @Autowired
+    private MessageServicePort messageServicePort;
+
+    @Autowired
+    private UserServicePort userServicePort;
+
+    @PostMapping("/webMessage/addUser")
+    public ResponseEntity<String> addUser(@RequestBody final UserDto userDto){
+        userServicePort.addUser(userDto);
+        return ResponseEntity.ok("");
+    }
+
+    @PostMapping("/webChat/connectToConversation")
+    public ResponseEntity<ConversationSubscriptionResponseDto> subscribeToConversation(@RequestBody final ConversationSubscriptionRequestDto conversationSubscriptionRequestDto){
+        ConversationSubscriptionResponseDto conversationSubscriptionResponseDto
+                = messageServicePort.subscribeToConversation(conversationSubscriptionRequestDto);
+        return ResponseEntity.ok(conversationSubscriptionResponseDto);
+    }
 }
---
+++ domain/src/main/java/ai/cresta/service/GenesysWebMessagingServiceImpl.java
@@ -0,0 +1,38 @@
+package ai.cresta.service;
+
+import ai.cresta.data.ConversationSubscriptionRequestDto;
+import ai.cresta.data.ConversationSubscriptionResponseDto;
+import ai.cresta.ports.api.MessageServicePort;
+import ai.cresta.ports.spi.GenesysPort;
+import lombok.AllArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+import java.text.MessageFormat;
+
+@AllArgsConstructor
+@Component(value="webMessaging")
+public class GenesysWebMessagingServiceImpl implements MessageServicePort {
+
+    private final SimpMessagingTemplate messageTemplate;
+    private final GenesysPort genesysPort;
+
+    @Override
+    public void notifyFrontend(String message) {
+        messageTemplate.convertAndSend("/topic/messages", message);
+
+    }
+
+    @Override
+    public void notifyUser(String id, String message) {
+        messageTemplate.convertAndSendToUser(id ,"/topic/private-messages", message);
+    }
+
+    @Override
+    public ConversationSubscriptionResponseDto subscribeToConversation(ConversationSubscriptionRequestDto conversationSubscriptionRequestDto){
+        String userId = conversationSubscriptionRequestDto.getUserId();
+        String conversationId = conversationSubscriptionRequestDto.getConversationId();
+        String topic = MessageFormat.format("v2.users.{0}.conversations.messages", userId);
+        return genesysPort.subscribeToConversation(userId, conversationId, topic);
+    }
+}
