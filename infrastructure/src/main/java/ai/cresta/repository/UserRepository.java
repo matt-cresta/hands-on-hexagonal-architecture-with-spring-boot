6bf6272051df8ddcabf3752ed80344d0e6abbbc5
Matthew DeGraffenreid <matthew.degraffenreid@cresta.ai>
2022-11-01 20:22:47 -0600

    add look up by userId

--- infrastructure/src/main/java/ai/cresta/repository/UserRepository.java
+++ infrastructure/src/main/java/ai/cresta/repository/UserRepository.java
@@ -2,9 +2,15 @@ package ai.cresta.repository;
 
 import ai.cresta.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-    
+    @Query("select u from User u where u.userId = :userId")
+    List<User> findByUserId(@Param("userId") String userId);
 }
