5ee83b19b96861bc11172072246df21f496d08c8
Matthew DeGraffenreid <matthew.degraffenreid@cresta.ai>
2022-11-03 22:38:39 -0600

    update user table for additional fields

--- infrastructure/src/main/resources/data.sql
+++ infrastructure/src/main/resources/data.sql
@@ -1,1 +1,6 @@
-CREATE TABLE IF NOT EXISTS USER (id number, userId varchar(250), platformName varchar(60), accessToken varchar(250), channelId varchar(250));
+CREATE TABLE IF NOT EXISTS USER (id number,
+                            userId varchar(250),
+                            platformName varchar(60),
+                            accessToken varchar(250),
+                            channelId varchar(250),
+                            conversationId varchar(250));
